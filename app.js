f8c5bfd52b1c3ad87f9ace14f9bdee83b7227319
Amazinggrace Oduye <grace.oduye@gmail.com>
2021-01-23 12:50:44 +0100

    bcrypt level

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-// require md5 hash function
-const md5 = require("md5");
+// require hash-salt bcrypt function
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -43,34 +44,40 @@ app.get("/register", function(req, res){
 
 // catch form post from register route
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    // create an hash password create a new user
+    bcrypt.hash( req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if (err){
+                console.log(err);
+            }else{
+                res.render("secrets")
+            };
+        });
+        
     });
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        }else{
-            res.render("secrets")
-        };
-    });
+   
 
 });
 // catch form post from login route
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
-
+    const password = req.body.password;
     // query user collection database
     User.findOne({email:username}, function(err, foundUser){
         if (err){
             console.log(err);
         }else{
-            // authenticate users
             if (foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets");
-                };
+                // compare hashed password with the login password
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true){
+                        res.render("secrets");   
+                    };
+                });
             };
         };
     });
