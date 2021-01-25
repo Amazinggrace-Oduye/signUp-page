862ee29c89c693690c5e9c2a7c5c5fdda1ede51e
Amazinggrace Oduye <grace.oduye@gmail.com>
2021-01-25 13:33:04 +0100

    passport-cookies level

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-// require hash-salt bcrypt function
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocaleMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
@@ -17,17 +18,37 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// set up express-session initial configuration
+app.use(session({
+    secret: 'our little secret.',
+    resave: false,
+    saveUninitialized: true
+}));
+// initialize passport to set up for authentication use
+app.use(passport.initialize());
+// use passport to set up persistent login  session
+app.use(passport.session());
+
 // connect mongoose
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.set('useCreateIndex', true)
 // create mongoose schema
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
+// plugin passport local mongoose to user schema
+// for hash and salting by passport and others
+userSchema.plugin(passportLocaleMongoose);
 
 // create mongoose model collection using schema
 const User = new mongoose.model("User", userSchema);
+// configure password local
+// create cookies using serialize
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // read route
 app.get("/", function(req, res){
@@ -41,46 +62,54 @@ app.get("/login", function(req, res){
 app.get("/register", function(req, res){
     res.render("register");
 });
+// render secret page if user is authenticated
+app.get("/secrets", function(req, res){
+    if (req.isAuthenticated){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    };
+});
 
-// catch form post from register route
+// catch log out route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+})
+// catch form post to register route
 app.post("/register", function(req, res){
-    // create an hash password create a new user
-    bcrypt.hash( req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-            if (err){
-                console.log(err);
-            }else{
-                res.render("secrets")
-            };
-        });
-        
-    });
-   
-
+    // use register a new user to database
+    User.register({username:req.body.username}, req.body.password, function(err, user){
+        if (err){
+            console.log(err);
+            res.redirect("/register")
+        }else{
+            // authentiacte user with local type of authentication
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+          })  
+        }
+    }) 
 });
 // catch form post from login route
 app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-    // query user collection database
-    User.findOne({email:username}, function(err, foundUser){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    // authenticate login user if the credentials alredy exist in database
+    //  
+    req.login(user, function(err){
         if (err){
             console.log(err);
         }else{
-            if (foundUser){
-                // compare hashed password with the login password
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result === true){
-                        res.render("secrets");   
-                    };
-                });
-            };
+            // the code below set up cookies 
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets")
+            });
         };
     });
+   
 });
 
 app.listen(3000, function(){
